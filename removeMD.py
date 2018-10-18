32a9225e40e51c029c3fdc0913fd4f976229e19e
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2018-10-18 11:54:47 -0300

    Delete removeMD.py

--- removeMD.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from pdf2image import convert_from_path, convert_from_bytes
-
-images = convert_from_path(r'C:\\Users\\Usuario\\Documents\\Python3_Projects\\splitPDF\\document-page0.pdf')
-images[0].save('test.png', 'png')
